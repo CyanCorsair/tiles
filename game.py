356273a198f490cf9f86f431a4ede76712f1c30b
Silver Salmu <silest@live.com>
2014-03-15 18:15:44 +0100

    It works
    Fixed game.py. It now renders a screen full of tiles.
    
    Acts as first prototype for upcoming changes:
    - Map scrolling
    - Map construction
    - Character movement

--- game.py
+++ game.py
@@ -9,51 +9,30 @@ SCREEN_WIDTH = 1024
 tile_width = 32
 tile_height = 32
 
-tile_batch = pyglet.graphics.Batch()
 map_batch = pyglet.graphics.Batch()
 
-tile1 = tile.Tile(img=resources.tile_one,x=0,y=0,batch=tile_batch)
-tile2 = tile.Tile(img=resources.tile_two,x=0,y=0,batch=tile_batch)
+tile1 = resources.tile_one
+tile2 = resources.tile_two
 
 tile_list = [tile1,tile2]
-map_count = []
 map = []
-
 
 window = pyglet.window.Window(SCREEN_WIDTH,SCREEN_HEIGHT)
 
-map_width = SCREEN_WIDTH / tile_width
-map_height = SCREEN_HEIGHT / tile_height
+map_width = 1280 / 32
+map_height = 1024 / 32
+map_size = (map_width * map_height)
 
-def getTilecount():
-	i = 0
-	
-	map_size = map_width * map_height
-	
-	while i < map_size:
-		i+= 1
-		map_count.append(i)
-		return map_count
-	
-	createTilemap()
-		
-def createTilemap():
-	for i in map_count:
-		tileimg = choice(tile_list)
-		if tileimg.x == SCREEN_WIDTH:
-			tileimg.x = -1
-			tileimg.y += tile_height
-			tileimg.x += tile_width
-		else:
-			tileimg.x += tile_width
-		map_batch
-	return map
+for a in xrange(map_height):
+	tile = choice(tile_list)
+	for j in xrange(map_width):
+		xpos = j * 32
+		ypos = a * 32
+		map.append(pyglet.sprite.Sprite(tile, xpos, ypos, batch=map_batch))
 		
 @window.event
 def on_draw():
-	#window.clear()
-	for tile in map:
-		tile.draw()
+	map_batch.draw()
 	
 if __name__ == '__main__':
 	pyglet.app.run()
